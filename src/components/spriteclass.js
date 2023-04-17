81be92249ed23a852908a94c35c082d18d1531fe
RohanGd <rohangadgil28@gmail.com>
2023-04-17 15:04:24 +0530

    added monsters who can speak messages

--- src/components/spriteclass.js
+++ src/components/spriteclass.js
@@ -1,69 +1,20 @@
 
 
-class sprite {
-    constructor (position, size) {
-      this.position = {
-        x: screen.width - width + position[0],
-        y: position[1],
-      };
-      this.width = size[0];
-      this.height = size[1];
-      this.speed = {
-        x: 0,
-        y: 0,
-      }
-
-      this.spriteImg = NaN;
-      this.gravity = true;
-      this.onGround = false;
-    }
-
-    update() {      
-      if (this.position.y + this.height*3/2 > height) {
-        this.position.y = height - this.height*3/2;
-        this.onGround = true;
-      }
-      if (this.onGround) 
-        this.speed.y = 0;
-
-      if (this.gravity && !this.onGround) 
-        this.speed.y += gravity;
-      this.position.x += this.speed.x;
-      this.position.y += this.speed.y;
 
 
-      this.draw();
-    }
+// import {ctx, sprite, gravity, width, height} from "../routes/+page.svelte"
 
-    draw() {
-      ctx.beginPath();
-      ctx.moveTo(this.position.x, this.position.y);
-      if (this.spriteImg == NaN) 
-      {
-        ctx.fillStyle = "red";
-        ctx.fillRect(this.position.x + this.width / 2 , this.position.y + this.height / 2, this.width, this.height);
-        console.log(this);
-      }
+// class monster extends sprite {
 
-      else {
-        // ctx.drawImage(this.spriteImg, this.position.x, this.position.y)
-        console.log(this.spriteImg);
-      }
-    }
+//   constructor(position, size, spt=NaN) {
+//     super(position, size, spt)
+//   }
 
-    setSpeedX(speed) {
-      this.speed.x = speed;      
-    } 
-    setSpeedY(speed) {
-      if (!this.onGround)
-        this.speed.y = speed;      
-    }
-
-    accelerate(speed) {
-      this.speed.x += speed[0];
-      this.speed.y += speed[1];
-    } 
-
-  };
+//   speak(text) {
+//     ctx.font = "48px sans-serif"
+//     ctx.fillText(text, this.position.x + 10, this.position.y + 10)
+//   }
+// };
 
 
+// export {monster};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,17 @@
   import PythonTerminalTest from "../components/PythonTerminalTest.svelte";
   import PythonTerminal from "../components/PythonTerminal.svelte";
   import { onMount, tick } from "svelte";
+  // import {monster} from "../components/spriteclass"
 
   const width = screen.width * 0.95;
-  const height = (screen.height * 0.7);
+  const height = screen.height * 0.7;
   let canvas, ctx;
   const gravity = 0.08;
 
   let myObject, objimg;
 
   class sprite {
-    constructor (position, size, spt=NaN) {
+    constructor(position, size, spt = NaN) {
       this.position = {
         x: screen.width - width + position[0],
         y: position[1],
@@ -21,46 +22,42 @@
       this.speed = {
         x: 0,
         y: 0,
-      }
-    
+      };
 
       this.spriteImg = spt;
       this.gravity = true;
       this.onGround = false;
     }
 
-    update() {   
+    update() {
       this.draw();
 
       if (this.position.y + this.height > height) {
         this.position.y = height - this.height;
         this.onGround = true;
       }
-      if (this.onGround) 
-        this.speed.y = 0;
+      if (this.onGround) this.speed.y = 0;
 
-      if (this.gravity && !this.onGround) 
-        this.speed.y += gravity;
+      if (this.gravity && !this.onGround) this.speed.y += gravity;
       this.position.x += this.speed.x;
       this.position.y += this.speed.y;
-
-
-      
     }
 
     draw() {
-      ctx.beginPath();
-      ctx.moveTo(this.position.x, this.position.y);
+      // ctx.beginPath();
+      // ctx.moveTo(this.position.x, this.position.y);
       // console.log(this.spriteImg);
 
-      if (typeof(this.spriteImg) == 'number') 
-      {
+      if (typeof this.spriteImg == "number") {
         ctx.fillStyle = "red";
-        ctx.fillRect(this.position.x + this.width / 2 , this.position.y + this.height / 2, this.width, this.height);
+        ctx.fillRect(
+          this.position.x + this.width / 2,
+          this.position.y + this.height / 2,
+          this.width,
+          this.height
+        );
         // console.log("not yo");
-      }
-
-      else {
+      } else {
         ctx.drawImage(this.spriteImg, this.position.x, this.position.y);
         // ctx.drawImage()
         // console.log("yo");
@@ -73,22 +70,33 @@
       this.onGround = false;
     }
     setSpeedX(speed) {
-      this.speed.x = speed;      
-    } 
+      this.speed.x = speed;
+    }
     setSpeedY(speed) {
       // if (!this.onGround)
-        this.speed.y = speed; 
-      this.onGround = false;     
+      this.speed.y = speed;
+      this.onGround = false;
     }
 
     accelerate(speed) {
       this.speed.x += speed[0];
       this.speed.y += speed[1];
-    } 
+    }
+  }
 
-  };
+  class monster extends sprite {
+    constructor(position, size, spt = NaN) {
+      super(position, size, spt);
+    }
 
+    speak(text) {
+      ctx.fillStyle = "white";
+      ctx.fillText(text, this.position.x, this.position.y - 10);
+      console.log(text, this.position.x);
+    }
+  }
 
+  // export {ctx, sprite, gravity, width, height};
 
   onMount(async () => {
     canvas = document.getElementById("gameCanvas");
@@ -96,43 +104,49 @@
     canvas.width = width;
     canvas.height = height;
 
-    
     objimg = document.querySelector("#playimg");
-    console.log(objimg);
-    myObject = new sprite([100,100],[113,200], objimg);
+    myObject = new monster([100, 100], [113, 200], objimg);
 
     await tick();
-    setTimeout(animate(),1000);
+    setTimeout(animate(), 1000);
     console.log(width, height);
-    myObject.setSpeedX(1);
+    myObject.setSpeedX(2);
     myObject.setSpeedY(-2);
-    
-
   });
 
-
-
+  var toAnimate = [];
   function animate() {
     window.requestAnimationFrame(animate);
     ctx.fillStyle = "black";
-    ctx.fillRect((screen.width - canvas.width), 0, canvas.width, canvas.height);
+    ctx.fillRect(screen.width - canvas.width, 0, canvas.width, canvas.height);
     myObject.update();
+    if (toAnimate.length > 0) {
+      toAnimate.forEach(function (parameters) {
+        let func = parameters[0];
+        let param = parameters.slice(1);
+        func(...param);
+      });
+    }
   }
 
   // setTimeout(()=> {myObject.moveTo([0,0])}, 5000);
-  setTimeout(()=> {myObject.setSpeedY(-3);}, 4000);
-  setTimeout(()=> {myObject.setSpeedX(0);}, 8000);
-
+  setTimeout(() => {
+    myObject.setSpeedY(-3);
+  }, 2000);
+  setTimeout(() => {
+    myObject.setSpeedX(0);
+  }, 4000);
+  setTimeout(() => {
+    toAnimate.push([myObject.speak.bind(myObject), "IAMHULK"]);
+  }, 5000);
+  // setTimeout(() => {
+  // toAnimate.pop();
+  // }, 7000);
 </script>
 
 <body class="bg-teal-500">
   <canvas id="gameCanvas" />
   <PythonTerminalTest />
 
-    <img id="playimg" src="src\assets\Hulk.jpg" alt="" class="hidden"/>
-  
+  <img id="playimg" src="src\assets\Hulk.jpg" alt="" class="hidden" />
 </body>
-
-
-
-
